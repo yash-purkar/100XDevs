0c2201e6d8abdf7fec4d527586909795fc893c7c
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-07-27 08:18:36 +0530

    Solved some questions in week 1

--- week-2/01-async-js/hard (promises)/CustomQuestion.js
+++ week-2/01-async-js/hard (promises)/CustomQuestion.js
@@ -1,1 +1,7 @@
 // Write a promise class by your own
+
+// promise takes a callback which has argument resolve and reject, if we call resolve it calls the .then and pass a resolve value to then's callback, and if reject it calls .catch and passes the reject's error to the callback of catch.
+
+
+
+// https://medium.com/nerd-for-tech/implement-your-own-promises-in-javascript-68ddaa6a5409
